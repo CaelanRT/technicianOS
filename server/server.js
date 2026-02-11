aced8bdcabf1db2d91bf96f42ab6b1313fa1f00b
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-02-10 21:57:44 -0500

    adding a simple server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+const express = require('express');
+
+const app = express();
+
+const port = 3000 || process.env.PORT;
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>hello world</h1>');
+})
+
+app.listen(port, () =>{
+    console.log('server is listening on port 3000...');
+})
+
