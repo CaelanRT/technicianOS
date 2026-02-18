b7fd0d4a0f30c2aa1f552d94b46b66a24ca215c6
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-02-18 14:48:02 -0500

    added delete project

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -1,15 +1,17 @@
+const { STATUS_CODES } = require('node:http');
 const { prisma } = require('../db/db');
 const {BadRequestError} = require('../errors/customError');
+const {StatusCodes} = require('http-status-codes');
 
-// get all tasks
+// get all Projects
 const getAllProjects = async (req, res) => {
     const projects = await prisma.project.findMany();
 
     if (!projects) {
-        res.status(400).json({msg: 'No projects in database'});
+        throw new BadRequestError('No Projects in database.')
     }
 
-    res.status(200).json({projects});
+    res.status(StatusCodes.OK).json({projects});
 }
 
 // get single project
@@ -36,15 +38,16 @@ const getSingleProject = async (req, res) => {
         throw new BadRequestError(`No Project with ID ${id}`);
     }
 
-    res.status(200).json({project});
+    res.status(StatusCodes.OK).json({project});
 }
 
+// create project
 const createProject = async (req, res) => {
 
     const {name, projectManagerId, technicianId} = req.body;
 
     if (!name || !projectManagerId || !technicianId) {
-        res.status(400).json({msg: 'Please send all parameters'});
+        throw new BadRequestError('Missing parameters.')
     }
 
     const project = await prisma.project.create({
@@ -56,14 +59,50 @@ const createProject = async (req, res) => {
     })
 
     if (!project) {
-        res.status(400).json({msg: 'Project could not be created'});
+        throw new BadRequestError('Project could not be created');
     }
     
-    res.status(200).json({project});
+    res.status(StatusCodes.OK).json({project});
+}
+
+// update project
+const updateProject = (req, res) => {
+    res.status(StatusCodes.OK).json({msg:'updateProject'});
+}
+
+// delete project
+const deleteProject = async (req, res) => {
+    let id = req.params.id;
+
+    if (!id) {
+        throw new BadRequestError('Missing ID. Please send with an ID.')
+    }
+
+    id = Number.parseInt(id)
+
+    if (Number.isNaN(id)) {
+        throw new BadRequestError('Invalid ID. Please send an integer value.');
+    }
+
+    // may need to check if the project is in the DB first then delete it
+
+    const project = await prisma.project.delete({
+        where : {
+            id: id
+        }
+    })
+
+    if (!project) {
+        throw new BadRequestError('No project with that ID');
+    }
+
+    res.status(StatusCodes.OK).json({project});
 }
 
 module.exports = {
     getAllProjects,
     getSingleProject,
-    createProject
+    createProject,
+    updateProject,
+    deleteProject
 }
--- server/routes/projectsRoutes.js
+++ server/routes/projectsRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {getAllProjects, getSingleProject, createProject} = require('../controllers/projectsController');
+const {getAllProjects, getSingleProject, createProject, updateProject, deleteProject} = require('../controllers/projectsController');
 const {getAllUsers, registerUser, loginUser} = require('../controllers/userController');
 const authenticateUser = require('../middleware/authentication');
 
 const router = express.Router();
 
 router.route('/projects').get(getAllProjects).post(createProject);
-router.route('/projects/:id').get(getSingleProject);
+router.route('/projects/:id').get(getSingleProject).patch(updateProject).delete(deleteProject);
 router.route('/users').get(authenticateUser, getAllUsers);
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
