db181f1c9f58529af2d2743759c36c776b71e413
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-03-12 17:15:14 -0400

    my LLM pipeline worksgit add .

--- server/controllers/webhookController.js
+++ server/controllers/webhookController.js
@@ -1,8 +1,11 @@
 const path =  require('path');
-const sendTranscriptToLLM = require('../services/transcriptServices');
+const {sendTranscriptToLLM, queryDBForTasks} = require('../services/transcriptServices');
+const { prisma } = require('../db/db');
+const { BadRequestError } = require('../errors/customError');
+const { StatusCodes } = require('http-status-codes');
 
 
-const processTranscript = (req, res) => {
+const processTranscript = async (req, res) => {
 
     // validate the request
 
@@ -13,16 +16,40 @@ const processTranscript = (req, res) => {
     // get the path of the file on the server
     const dataPath = path.join(__dirname, '../dummy-data/example.vtt');
 
+    //figure out what customer/project we're talking about
+
+    // query the db for all the tasks related to that project
+    const tasks = await queryDBForTasks(1);
+    
     // call the upload transcript, giving it the path of the file to upload and the tasks on the project this is affiliated with
-    const llmOutput = sendTranscriptToLLM(dataPath);
+    const llmOutput = await sendTranscriptToLLM(dataPath, tasks);
 
     // get the return from the LLM of the tasks it thinks it completed based on the transcript
+    const result = JSON.parse(llmOutput);
+
+    const tasksArray = result.tasks;
+
+    const tasksFiltered = tasksArray.filter((t) => t.completed === true && t.certainty > 80);
 
     // update the tasks
+    for (i = 0; i < tasksFiltered.length; ++i) {
+        let updatedTask = await prisma.task.update({
+            where: {
+                id: tasksFiltered[i].id
+            },
+            data: {
+                status: "completed"
+            }
+        })
 
+        // maybe placeholder error for now
+        if (!updatedTask) {
+            throw new BadRequestError('Unable to complete request');
+        }
+    }
     // send the response
 
-    res.send('process transcript');
+    res.status(StatusCodes.OK).json({msg:`Successfully updated ${tasksFiltered.length} tasks`});
 }
 
 module.exports = processTranscript;
--- server/services/transcriptServices.js
+++ server/services/transcriptServices.js
@@ -1,23 +1,46 @@
 const {OpenAI} = require('openai');
 const fs = require('fs');
+const { prisma } = require('../db/db');
+const {BadRequestError} = require('../errors/customError');
 
 const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-// could probably create a service that reads the file and sends it back
+// service that queries the DB for the project tasks
+const queryDBForTasks = async (projectId) => {
+    const tasks = await prisma.task.findMany({
+        where: {
+            projectId: projectId
+        }
+    })
+
+    // this is a placeholder, need to figure out throwing an error and retry stuff for here
+    if (!tasks) {
+        throw new BadRequestError('no tasks with that ID');
+    }
+
+    const tasksArray = [];
+
+    for (i = 0; i < tasks.length; ++i) {
+        let templateTask = {
+            id: tasks[i].id,
+            name: tasks[i].name,
+            description: tasks[i].description
+        };
+
+        tasksArray.push(templateTask);
+    }
+
+    console.log(tasksArray);
+
+    return tasksArray;
+    
+}
 
 // service that gets the contents of the file and sends to llm, returns the response
-const sendTranscriptToLLM = async (dataPath) => {
+const sendTranscriptToLLM = async (dataPath, tasksArray) => {
 
-
-    // Test hitting the api
-    /*const response = await client.responses.create({
-        model: "gpt-5.4",
-        input: "Write a one-sentence bedtime story about a unicorn."
-    });
-    
-    console.log(response.output_text);*/
 
     //upload file
     const file = await client.files.create({
@@ -38,7 +61,27 @@ const sendTranscriptToLLM = async (dataPath) => {
                     },
                     {
                         type: "input_text",
-                        text: "This file is a transcription of a zoom call recording between an implementation specialist and a customer. Please read this transcription and compile a list of all tasks completed on this service based on what is said in the transcription.",
+                        text: `
+                        Objective:
+                        You are tasked with reading a transcript and comparing it to a list of tasks formatted in JSON format.
+
+                        Input:
+                        - uploaded input file
+                        - JSON Array ${JSON.stringify(tasksArray, null, 2)}
+                        
+                        Directives:
+                        The goal is to read the file that is the transcript and decipher from that transcript what tasks were completed, based on the tasks that are being given to you in the JSON Array from the input section. The input file is a transcription of a zoom call recording between an implementation specialist and a customer. Read the JSON input of the tasks, then read the transcript and keep a list of what tasks you can decipher were completed based on reading the call transcript. 
+                        
+                        Output:
+                        Output your findings in the following JSON format -
+                        tasks: [
+                            {
+                                id: id of task taken from input (int),
+                                completed: 'true' or 'false' (boolean),
+                                certainty: number between 1 - 100 with 100 being absolutely positive the task was complete (int)
+                            }
+                        ]
+                        `,
                     },
                 ],
             }
@@ -46,7 +89,7 @@ const sendTranscriptToLLM = async (dataPath) => {
     })
 
     // get response
-    console.log(response.output_text);
+    //console.log(response.output_text);
 
     return response.output_text;
     
@@ -54,4 +97,4 @@ const sendTranscriptToLLM = async (dataPath) => {
 
 // service that does stuff with the tasks!
 
-module.exports = sendTranscriptToLLM;
+module.exports = {sendTranscriptToLLM, queryDBForTasks};
